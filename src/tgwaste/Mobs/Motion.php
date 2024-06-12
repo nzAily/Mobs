3457ea690e6009c23717385f48a87f3bf79ea831
RB007 <36972202+RoosterBooster007@users.noreply.github.com>
2024-06-12 18:57:22 -0400

    Update Motion.php

--- src/tgwaste/Mobs/Motion.php
+++ src/tgwaste/Mobs/Motion.php
@@ -4,187 +4,112 @@ declare(strict_types=1);
 
 namespace tgwaste\Mobs;
 
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\math\Vector3;
+use pocketmine\event\entity\EntityDespawnEvent;
+use pocketmine\event\entity\EntityDeathEvent;
+use pocketmine\event\entity\EntitySpawnEvent;
+use pocketmine\event\player\PlayerItemUseEvent;
 use pocketmine\player\Player;
-use tgwaste\Mobs\Entities\MobsEntity;
+use pocketmine\item\StringToItemParser;
+use tgwaste\Mobs\Entities\Cow;
+use tgwaste\Mobs\Registrations;
 
-class Motion {
-	public function tick(MobsEntity $entity) {
-		$timer = $entity->getTimer() - 1;
-		$flying = $entity->isFlying();
 
-		$entity->setTimer($timer);
-
-		if ($timer > 0) {
-			return $this->wait($entity);
-		}
-
-		if ($timer == 0 and $flying == false and mt_rand(0, 1) == 1 and $entity->getTargetEntity() === null) {
-			return $entity->setTimer(mt_rand(100, 600));
-		}
-
-		$pos = $entity->getDestination();
-
-		if (!$pos->x and !$pos->y and !$pos->z) {
-			$entity->setDestination(Main::$instance->coordsobj->getRandomDestination($entity));
-		}
-
-		if ($entity->getTimer() > 0) {
-			return;
-		}
-
-		$this->move($entity);
+class Listen implements Listener {
+	public function onPlayerJoinEvent(PlayerJoinEvent $event) {
+		//Registrations::registerSpawnEggs($event->getPlayer());
+		// to be added in the future
 	}
+	
+	public function onEntityDamageByEntityEvent(EntityDamageByEntityEvent $event) {
+		$entity = $event->getEntity();
 
-	public function move(MobsEntity $entity) {
-		$motion = $entity->getMotion();
-		$location = $entity->getLocation();
-		$swimming = $entity->isSwimming();
-		$flying = $entity->isFlying();
-
-		if (!$entity->onGround and $motion->y < 0 and $flying == false and $swimming == false) {
-			$motion->y *= 0.6;
-		}
-
-		else {
-			if (mt_rand(0, 500) == 1 or ($entity->isCollided == true and $swimming == true)) {
-				# random chance of getting a new destination
-				$entity->setDestination(Main::$instance->coordsobj->getRandomDestination($entity));
-			}
-			$targetpos = $this->calculateMotion($entity);
-			$motion->x = $targetpos->x;
-			$motion->y = $targetpos->y;
-			$motion->z = $targetpos->z;
-		}
-
-		if ($entity->getTimer() > 0) {
-			return;
-		}
-
-		if ($entity->isCollidedHorizontally == true and $swimming == false) {
-			$motion->y = 1;
-		}
-
-		if ($entity->isJumping() == true and $entity->onGround) {
-			$motion->y = 1;
-		}
-
-		if ($entity->getName() === "Chicken" and $entity->onGround and mt_rand(0, 100) >= 99) {
-			$motion->y = 1;
-		}
-
-		$vec = new Vector3($motion->x, $motion->y, $motion->z);
-		$look = new Vector3($location->x+$motion->x, $location->y+$motion->y+$entity->getEyeHeight(), $location->z+$motion->z);
-
-		$entity->setDefaultLook($look);
-
-		$entity->lookAt($look);
-		$entity->setMotion($vec);
-		$this->attackEntity($entity, 4);
-	}
-
-	public function wait(MobsEntity $entity) {
-		$location = $entity->getLocation();
-
-		if ($entity->lastUpdate % 100 == 0) {
-			if ($entity->getHealth() < $entity->getMaxHealth()) {
-				$entity->setHealth($entity->getHealth() + Main::$instance->regainhealth);
-			}
-			$entity->damageTag();
-		}
-
-		if ($entity->isFlying() == true) {
-			return;
-		}
-
-		if ($entity->isSwimming() == true) {
-			if (!$entity->isUnderwater()) {
-				$entity->setTimer(-1);
-			}
-			return;
-		}
-
-		if ($entity->catchesFire() == true and Main::$instance->toolsobj->isDayTime($entity->getWorld())) {
-			$entity->setOnFire(120);
-			$entity->setTargetEntity($entity);
-		}
-
-		if ($entity->isOnFire()) {
-			$this->attackEntity($entity, 4);
-		}
-
-		if (mt_rand(1, 200) == 1) {
-			$entity->lookAt($entity->getDefaultLook());
-			return;
-		}
-
-		if (mt_rand(1, 200) == 1) {
-			$x = $location->x + mt_rand(-1, 1);
-			$y = $location->y + mt_rand(-1, 1);
-			$z = $location->z + mt_rand(-1, 1);
-			$entity->lookAt(new Vector3($x, $y, $z));
+		if (!($entity instanceof Player) && Main::$instance->toolsobj->pluginMobExists($entity->getNetworkTypeId())) {
+			$entity->setScoreTag("1"); // to record player hit for xp
 		}
 	}
 
-	public function calculateMotion(MobsEntity $entity) : Vector3 {
-		$dest = $entity->getDestination();
-		$epos = $entity->getPosition();
-		$motion = $entity->getMotion();
-		$speed = $entity->getMovementSpeed();
-		$speed = 1.0;
-		$flying = $entity->isFlying();
-
-		$x = $dest->x - $epos->x;
-		$y = $dest->y - $epos->y;
-		$z = $dest->z - $epos->z;
-
-		if ($x ** 2 + $z ** 2 < 0.7) {
-			if ($entity->getTargetEntity() === null) {
-				$motion->y = 0;
-				$entity->setTimer($flying == true ? 100 : 200);
-				$entity->setDestination(new Vector3(0, 0, 0));
-			}
-		} else {
-			$diff = abs($x) + abs($z);
-
-			$motion->x = $speed * 0.15 * ($x / $diff);
-			$motion->y = 0;
-			$motion->z = $speed * 0.15 * ($z / $diff);
-
-			if ($entity->isSwimming()) {
-				$motion->y = $speed * 0.15 * ($y / $diff);
-			}
+	public function onEntityDamageEvent(EntityDamageEvent $event) {
+		$entity = $event->getEntity();
+		
+		if ($entity instanceof MobsEntity) {
+			$entity->damageTag(); // updates tags more often (like when a mob is damaged in fire)
 		}
-
-		return new Vector3($motion->x, $motion->y, $motion->z);
 	}
 
-	public function attackEntity(MobsEntity $entity, int $damage) {
-		$target = $entity->getTargetEntity();
+	public function onEntityDespawnEvent(EntityDespawnEvent $event) {
+		$entity = $event->getEntity();
+		if (method_exists($entity, "getName") and $entity instanceof Entity) {
+			Main::$instance->toolsobj->spawnMessage($entity, "Despawned");
+		}
+	}
 
-		if ($target === null) {
-			return;
+	public function onEntityDeathEvent(EntityDeathEvent $event) {
+		$entity = $event->getEntity();
+		if (method_exists($entity, "getName") and $entity instanceof Entity) {
+			Main::$instance->toolsobj->spawnMessage($entity, "Died");
 		}
 
-		$dist = $entity->getPosition()->distanceSquared($target->getPosition());
+		if (Main::$instance->mobdrops && Main::$instance->toolsobj->pluginMobExists($entity->getNetworkTypeId())) {
+			$mobclass = Main::$instance->toolsobj->getPluginMobClass($entity->getNetworkTypeId());
+			try {
+				$drops = [];
+				foreach ($mobclass::drops as $di) {
+					$chance = mt_rand(1, 1000) / 10.0;
+					if ($chance <= $di[2]) {
+						$numitems = mt_rand(1, $di[1]);
+						if ($numitems == 0) {
+							$numitems++; // always at least one item
+						}
 
-		if (!$target->isAlive() or $dist >= 200 or ($target instanceof Player and $target->isCreative() == true)) {
-			$entity->setMovementSpeed(1.00);
-			$entity->setTargetEntity(null);
-			return;
+						// fire and fire aspect
+						if ($entity->isOnFire()) {
+							$di[0] = str_replace("RAW", "COOKED", $di[0]);
+						}
+
+						$item = StringToItemParser::getInstance()->parse($di[0]);
+						$item->setCount($numitems);
+						array_push($drops, $item);
+					}
+				}
+				$event->setDrops($drops);
+
+				if (Main::$instance->mobexp && $entity->getScoreTag() == "1") {
+					$usualxp = $mobclass::xpamount;
+					$minXp = (int) floor($usualxp * 0.5);
+					$maxXp = (int) ceil($usualxp * 1.5);
+					$event->setXpDropAmount(mt_rand($minXp, $maxXp));
+				}
+			} catch (\Exception $e) {
+				Main::$instance->getServer()->getLogger()->info("Item/XP Drop ERR: " . $e);
+			}
+		}
+	}
+
+	public function onEntitySpawnEvent(EntitySpawnEvent $event) {
+		$entity = $event->getEntity();
+		if ($entity instanceof Entity) {
+			Main::$instance->toolsobj->spawnMessage($entity, "Spawned");
+		}
+	}
+
+	public function onPlayerItemUseEvent(PlayerItemUseEvent $event) {
+		$player = $event->getPlayer();
+		$item = $event->getItem();
+		$direction = $event->getDirectionVector();
+
+		// simplist milk logic fr (anyone wanna deal with raycasts?)
+		if ($item->getVanillaName() == "Bucket" && $player->getWorld()->getNearestEntity($player->getLocation(), 6, Cow::class, false)) {
+			$player->getInventory()->setItemInHand(StringToItemParser::getInstance()->parse("MILK_BUCKET"));
 		}
 
-		if ($entity->getAttackDelay() > 20 && $dist < 2) {
-			$entity->setAttackDelay(0);
-			$ev = new EntityDamageByEntityEvent($entity, $target, EntityDamageEvent::CAUSE_ENTITY_ATTACK, $damage);
-			$target->attack($ev);
+		// using left click for tags :skull
+		if ($item->getVanillaName() == "Name Tag") {
+			$player->sendMessage("§eLeft click mob to apply name tag!");	
 		}
-
-		$entity->setAttackDelay($entity->getAttackDelay() + 1);
-
-		$pos = $target->getPosition();
-		$entity->setDestination(new Vector3($pos->x, 0, $pos->z));
-  }
+	}
 }
